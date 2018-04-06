2f2eea63f6cc99040213c1eca2749ba9e1e3bb4a
paintanddraw <lhpaint@163.com>
2018-04-06 13:09:36 +0800

    add mbg project

---
+++ ssm/src/test/java/com/oaec/test/AppTest.java
@@ -0,0 +1,34 @@
+package com.oaec.test;
+
+import com.oaec.entity.User;
+import com.oaec.mapper.UserMapper;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.Reader;
+
+public class AppTest {
+    public static void main(String[] args) throws IOException {
+        //加载mybatis配置文件
+        Reader reader = Resources.getResourceAsReader( "mybatis-config.xml" );
+        //创建回话工厂
+        SqlSessionFactory sessionFactory = new SqlSessionFactoryBuilder().build( reader );
+        //创建会话
+        SqlSession session = sessionFactory.openSession();
+       /* //创建语句
+        String statememt = "com.oaec.mapper.UserMapper.getById";
+        //
+        User user  = session.selectOne( statememt,1 );*/
+        User user = new User( "tom", 23 );
+        int result = session.getMapper( UserMapper.class ).insert( user );
+        System.out.println(result);
+        session.commit();
+
+    }
+
+
+}
+
